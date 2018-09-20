fb8a29c99423dd1cf9fa6e710e43c616f05b4bb1
RomeoKilo125 <romeokilo125@gmail.com>
2018-09-20 11:19:52 -0400

    loading in answers, and showing answers on click

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  console.log("ready")
+
+// start game - reset score, load question
+
+// 
+
   let answers = [
-    '1',
+    'answer one',
     '2',
     '3',
     '4',
@@ -17,13 +21,30 @@ $(document).ready(function() {
     let answerArea = $('#answerArea')
     array.forEach(function(answer, i) {
       let newdiv = $('<div>')
+      newdiv.attr("id", `ans${i}`)
+      newdiv.attr("data-id", `${i}`)
+      newdiv.addClass("hide answer")
       newdiv.text(`${answer}`)
-      newdiv.attr("id", `cvr${i}`)
-      newdiv.attr("class", "cover")
+      let coverdiv = $(`<div>`)
+      coverdiv.attr("id", `cvr${i}`)
+      coverdiv.attr("data-id", `${i}`)
+      coverdiv.addClass("cover")
+      coverdiv.text(`${i + 1}`)
+      answerArea.append(coverdiv)
       answerArea.append(newdiv)
     })
-
   }
+
+  function showAnswer(id) {
+    console.log(`show answer ${id}`)
+    $(`#cvr${id}`).addClass("hide")
+    $(`#ans${id}`).removeClass("hide")
+  }
+
+  $("body").on("click", ".cover", function() {
+    let id = $(this).attr("data-id")
+    showAnswer(id)
+  })
 
   addAnswers(answers)
 })
