e1cd094fb9449df8069e0cb67b412ac515b7d784
RomeoKilo125 <romeokilo125@gmail.com>
2018-11-28 23:24:16 -0500

    began adding christmas questions

---
+++ js/winterQuestions.js
@@ -0,0 +1,299 @@
+let questions = [
+  {
+    q:`Name something at the top of a woman's Christmas list.`,
+    a:[
+      {
+        content: `Jewelry`,
+        value: 41
+      }, {
+        content: `Clothes`,
+        value: 11
+      }, {
+        content: `Diamonds`,
+        value: 8
+      }, {
+        content: `Spa Treatments`,
+        value: 6
+      }, {
+        content: `Vacation/day off`,
+        value: 6
+      }, {
+        content: `Shoes`,
+        value: 5
+      }, {
+        content: `Perfume`,
+        value: 5
+      }, {
+        content: `Money`,
+        value: 3
+      }, {
+        content: `Appliance`,
+        value: 2
+      }, {
+        content: `Slippers`,
+        value: 2
+      }
+    ]
+  }, {
+    q: `Name something associate with the classic Christmas movie "National Lampoon's Christmas Vacation".`,
+    a: [
+      {
+        content: `Christmas lights`,
+        value: 36
+      }, {
+        content: `Cousin Eddy`,
+        value: 7
+      }, {
+        content: `Electrocuted cat`,
+        value: 6
+      }, {
+        content: `Squirrel`,
+        value: 6
+      }, {
+        content: `Christmas tree`,
+        value: 6
+      }, {
+        content: `RV`,
+        value: 6
+      }, {
+        content: `Family`,
+        value: 5
+      }, {
+        content: `Mele Kalikimaka pool scene`,
+        value: 5
+      }
+    ]
+  }, {
+    q: `Name a popular Christmas carol.`,
+    a: [
+      {
+        content: `Silent Night`,
+        value: 22
+      }, {
+        content: `Jingle Bells`,
+        value: 20
+      }, {
+        content: `Joy to the World`,
+        value: 10
+      }, {
+        content: `O Holy Night`,
+        value: 8
+      }, {
+        content: `Deck the Halls`,
+        value: 5
+      }, {
+        content: `The First Noel`,
+        value: 4
+      }, {
+        content: `Little Drummer Boy`,
+        value: 3
+      }, {
+        content: `We Wish You a Merry Christmas`,
+        value: 3
+      }, {
+        content: `Rudolph the Red Nosed Reindeer`,
+        value: 3
+      }
+    ]
+  }, {
+    q: `Name a tradition that makes it feel most like the Christmas season.`,
+    a: [
+      {
+        content: `Decorating/putting up a Christmas tree`,
+        value: 33
+      }, {
+        content: `Baking goodies`,
+        value: 13
+      }, {
+        content: `Caroling`,
+        value: 12
+      }, {
+        content: `Going to get the Christmas tree`,
+        value: 5
+      }, {
+        content: `Driving around to see Christmas lights`,
+        value: 5
+      }, {
+        content: `Decorating the house`,
+        value: 5
+      }, {
+        content: `Drinking eggnog`,
+        value: 4
+      }, {
+        content: `Listening to Christmas music`,
+        value: 3
+      }, {
+        content: `Exchanging gifts`,
+        value: 3
+      }
+    ]
+  }, {
+    q: `Name something you avoid during the holidays.`,
+    a: [
+      {
+        content: `The mall`,
+        value: 34
+      }, {
+        content: `Shopping`,
+        value: 15
+      }, {
+        content: `Stores`,
+        value: 6
+      }, {
+        content: `Crowds`,
+        value: 6
+      }, {
+        content: `Scale`,
+        value: 5
+      }, {
+        content: `Black Friday`,
+        value: 4
+      }, {
+        content: `Traffic`,
+        value: 4
+      }
+    ]
+  }, {
+    q: `Name a favoite holiday food.`,
+    a: [
+      {
+        content: `Cookies`,
+        value: 18
+      }, {
+        content: `Ham`,
+        value: 16
+      }, {
+        content: `Fudge`,
+        value: 11
+      }, {
+        content: `Eggnog`,
+        value: 8
+      }, {
+        content: `Hot Chocolate`,
+        value: 6
+      }, {
+        content: `Tamales`,
+        value: 5
+      }, {
+        content: `Pie`,
+        value: 5
+      }, {
+        content: `Turkey`,
+        value: 5
+      }, {
+        content: `Prime Rib`,
+        value: 4
+      }
+    ]
+  }, {
+    q: `Name something you wanted from Santa as a child.`,
+    a: [
+      {
+        content: `Bike`,
+        value: 17
+      }, {
+        content: `Doll`,
+        value: 9
+      }, {
+        content: `Nintendo/Video Games`,
+        value: 8
+      }, {
+        content: `Cabbage Patch Doll`,
+        value: 7
+      }, {
+        content: `Dollhouse/Barbie Dream House`,
+        value: 6
+      }, {
+        content: `Action figures`,
+        value: 5
+      }, {
+        content: `Horse`,
+        value: 5
+      }, {
+        content: `Barbie`,
+        value: 5
+      }, {
+        content: `Rollerskates`,
+        value: 4
+      }, {
+        content: `Puppy`,
+        value: 3
+      }
+    ]
+  }, {
+    q: `Name an item likely to be found in a Christmas stocking as a gift from Santa`,
+    a: [
+      {
+        content: `Candy`,
+        value: 31
+      }, {
+        content: `Orange`,
+        value: 13
+      }, {
+        content: `Candy cane`,
+        value: 11
+      }, {
+        content: `Toiletries`,
+        value: 10
+      }, {
+        content: `Socks`,
+        value: 8
+      }, {
+        content: `Chocolate`,
+        value: 4
+      }, {
+        content: `Underwear`,
+        value: 3
+      }, {
+        content: `PEZ Dispenser`,
+        value: 3
+      }, {
+        content: `DVD Movie`,
+        value: 3
+      }
+    ]
+  }, {
+    q: `Name a movie star with whom you'd like to be caught under the mistletoe. (female responses)`,
+    a: [
+      {
+        content: `Ryan Gosling`,
+        value: 7
+      }, {
+        content: `Matthew McConaughey`,
+        value: 6
+      }, {
+        content: `Hugh Jackman`,
+        value: 5
+      }, {
+        content: `George Clooney`,
+        value: 5
+      }, {
+        content: `Johnny Depp`,
+        value: 5
+      }, {
+        content: `Matt Damon`,
+        value: 4
+      }, {
+        content: `Patrick Dempsey`,
+        value: 4
+      }, {
+        content: `Ryan Reynolds`,
+        value: 4
+      }, {
+        content: `Christian Bale`,
+        value: 3
+      }, {
+        content: `Robert Pattinson`,
+        value: 3
+      }
+    ]
+  }, {
+    q: `Name a movie star with whom you'd like to be caught under the mistletoe. (male responses)`,
+    a: [
+      {
+        content: ``,
+        value:
+      }
+    ]
+  }
+]
