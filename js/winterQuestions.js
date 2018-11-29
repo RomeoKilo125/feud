314378a0e2a111e2601391f73bd19c36824d0611
RomeoKilo125 <romeokilo125@gmail.com>
2018-11-29 15:33:18 -0500

    More winter questions

--- js/winterQuestions.js
+++ js/winterQuestions.js
@@ -1,299 +1,606 @@
-let questions = [
-  {
-    q:`Name something at the top of a woman's Christmas list.`,
-    a:[
-      {
-        content: `Jewelry`,
-        value: 41
-      }, {
-        content: `Clothes`,
-        value: 11
-      }, {
-        content: `Diamonds`,
-        value: 8
-      }, {
-        content: `Spa Treatments`,
-        value: 6
-      }, {
-        content: `Vacation/day off`,
-        value: 6
-      }, {
-        content: `Shoes`,
-        value: 5
-      }, {
-        content: `Perfume`,
-        value: 5
-      }, {
-        content: `Money`,
-        value: 3
-      }, {
-        content: `Appliance`,
-        value: 2
-      }, {
-        content: `Slippers`,
-        value: 2
-      }
-    ]
+let questions = [{
+  q: `Name something at the top of a woman's Christmas list.`,
+  a: [{
+    content: `Jewelry`,
+    value: 41
   }, {
-    q: `Name something associate with the classic Christmas movie "National Lampoon's Christmas Vacation".`,
-    a: [
-      {
-        content: `Christmas lights`,
-        value: 36
-      }, {
-        content: `Cousin Eddy`,
-        value: 7
-      }, {
-        content: `Electrocuted cat`,
-        value: 6
-      }, {
-        content: `Squirrel`,
-        value: 6
-      }, {
-        content: `Christmas tree`,
-        value: 6
-      }, {
-        content: `RV`,
-        value: 6
-      }, {
-        content: `Family`,
-        value: 5
-      }, {
-        content: `Mele Kalikimaka pool scene`,
-        value: 5
-      }
-    ]
+    content: `Clothes`,
+    value: 11
   }, {
-    q: `Name a popular Christmas carol.`,
-    a: [
-      {
-        content: `Silent Night`,
-        value: 22
-      }, {
-        content: `Jingle Bells`,
-        value: 20
-      }, {
-        content: `Joy to the World`,
-        value: 10
-      }, {
-        content: `O Holy Night`,
-        value: 8
-      }, {
-        content: `Deck the Halls`,
-        value: 5
-      }, {
-        content: `The First Noel`,
-        value: 4
-      }, {
-        content: `Little Drummer Boy`,
-        value: 3
-      }, {
-        content: `We Wish You a Merry Christmas`,
-        value: 3
-      }, {
-        content: `Rudolph the Red Nosed Reindeer`,
-        value: 3
-      }
-    ]
+    content: `Diamonds`,
+    value: 8
   }, {
-    q: `Name a tradition that makes it feel most like the Christmas season.`,
-    a: [
-      {
-        content: `Decorating/putting up a Christmas tree`,
-        value: 33
-      }, {
-        content: `Baking goodies`,
-        value: 13
-      }, {
-        content: `Caroling`,
-        value: 12
-      }, {
-        content: `Going to get the Christmas tree`,
-        value: 5
-      }, {
-        content: `Driving around to see Christmas lights`,
-        value: 5
-      }, {
-        content: `Decorating the house`,
-        value: 5
-      }, {
-        content: `Drinking eggnog`,
-        value: 4
-      }, {
-        content: `Listening to Christmas music`,
-        value: 3
-      }, {
-        content: `Exchanging gifts`,
-        value: 3
-      }
-    ]
+    content: `Spa Treatments`,
+    value: 6
   }, {
-    q: `Name something you avoid during the holidays.`,
-    a: [
-      {
-        content: `The mall`,
-        value: 34
-      }, {
-        content: `Shopping`,
-        value: 15
-      }, {
-        content: `Stores`,
-        value: 6
-      }, {
-        content: `Crowds`,
-        value: 6
-      }, {
-        content: `Scale`,
-        value: 5
-      }, {
-        content: `Black Friday`,
-        value: 4
-      }, {
-        content: `Traffic`,
-        value: 4
-      }
-    ]
+    content: `Vacation/day off`,
+    value: 6
   }, {
-    q: `Name a favoite holiday food.`,
-    a: [
-      {
-        content: `Cookies`,
-        value: 18
-      }, {
-        content: `Ham`,
-        value: 16
-      }, {
-        content: `Fudge`,
-        value: 11
-      }, {
-        content: `Eggnog`,
-        value: 8
-      }, {
-        content: `Hot Chocolate`,
-        value: 6
-      }, {
-        content: `Tamales`,
-        value: 5
-      }, {
-        content: `Pie`,
-        value: 5
-      }, {
-        content: `Turkey`,
-        value: 5
-      }, {
-        content: `Prime Rib`,
-        value: 4
-      }
-    ]
+    content: `Shoes`,
+    value: 5
   }, {
-    q: `Name something you wanted from Santa as a child.`,
-    a: [
-      {
-        content: `Bike`,
-        value: 17
-      }, {
-        content: `Doll`,
-        value: 9
-      }, {
-        content: `Nintendo/Video Games`,
-        value: 8
-      }, {
-        content: `Cabbage Patch Doll`,
-        value: 7
-      }, {
-        content: `Dollhouse/Barbie Dream House`,
-        value: 6
-      }, {
-        content: `Action figures`,
-        value: 5
-      }, {
-        content: `Horse`,
-        value: 5
-      }, {
-        content: `Barbie`,
-        value: 5
-      }, {
-        content: `Rollerskates`,
-        value: 4
-      }, {
-        content: `Puppy`,
-        value: 3
-      }
-    ]
+    content: `Perfume`,
+    value: 5
   }, {
-    q: `Name an item likely to be found in a Christmas stocking as a gift from Santa`,
-    a: [
-      {
-        content: `Candy`,
-        value: 31
-      }, {
-        content: `Orange`,
-        value: 13
-      }, {
-        content: `Candy cane`,
-        value: 11
-      }, {
-        content: `Toiletries`,
-        value: 10
-      }, {
-        content: `Socks`,
-        value: 8
-      }, {
-        content: `Chocolate`,
-        value: 4
-      }, {
-        content: `Underwear`,
-        value: 3
-      }, {
-        content: `PEZ Dispenser`,
-        value: 3
-      }, {
-        content: `DVD Movie`,
-        value: 3
-      }
-    ]
+    content: `Money`,
+    value: 3
   }, {
-    q: `Name a movie star with whom you'd like to be caught under the mistletoe. (female responses)`,
-    a: [
-      {
-        content: `Ryan Gosling`,
-        value: 7
-      }, {
-        content: `Matthew McConaughey`,
-        value: 6
-      }, {
-        content: `Hugh Jackman`,
-        value: 5
-      }, {
-        content: `George Clooney`,
-        value: 5
-      }, {
-        content: `Johnny Depp`,
-        value: 5
-      }, {
-        content: `Matt Damon`,
-        value: 4
-      }, {
-        content: `Patrick Dempsey`,
-        value: 4
-      }, {
-        content: `Ryan Reynolds`,
-        value: 4
-      }, {
-        content: `Christian Bale`,
-        value: 3
-      }, {
-        content: `Robert Pattinson`,
-        value: 3
-      }
-    ]
+    content: `Appliance`,
+    value: 2
   }, {
-    q: `Name a movie star with whom you'd like to be caught under the mistletoe. (male responses)`,
-    a: [
-      {
-        content: ``,
-        value:
-      }
-    ]
-  }
-]
+    content: `Slippers`,
+    value: 2
+  }]
+}, {
+  q: `Name something associate with the classic Christmas movie "National Lampoon's Christmas Vacation".`,
+  a: [{
+    content: `Christmas lights`,
+    value: 36
+  }, {
+    content: `Cousin Eddy`,
+    value: 7
+  }, {
+    content: `Electrocuted cat`,
+    value: 6
+  }, {
+    content: `Squirrel`,
+    value: 6
+  }, {
+    content: `Christmas tree`,
+    value: 6
+  }, {
+    content: `RV`,
+    value: 6
+  }, {
+    content: `Family`,
+    value: 5
+  }, {
+    content: `Mele Kalikimaka pool scene`,
+    value: 5
+  }]
+}, {
+  q: `Name a popular Christmas carol.`,
+  a: [{
+    content: `Silent Night`,
+    value: 22
+  }, {
+    content: `Jingle Bells`,
+    value: 20
+  }, {
+    content: `Joy to the World`,
+    value: 10
+  }, {
+    content: `O Holy Night`,
+    value: 8
+  }, {
+    content: `Deck the Halls`,
+    value: 5
+  }, {
+    content: `The First Noel`,
+    value: 4
+  }, {
+    content: `Little Drummer Boy`,
+    value: 3
+  }, {
+    content: `We Wish You a Merry Christmas`,
+    value: 3
+  }, {
+    content: `Rudolph the Red Nosed Reindeer`,
+    value: 3
+  }]
+}, {
+  q: `Name a tradition that makes it feel most like the Christmas season.`,
+  a: [{
+    content: `Decorating/putting up a Christmas tree`,
+    value: 33
+  }, {
+    content: `Baking goodies`,
+    value: 13
+  }, {
+    content: `Caroling`,
+    value: 12
+  }, {
+    content: `Going to get the Christmas tree`,
+    value: 5
+  }, {
+    content: `Driving around to see Christmas lights`,
+    value: 5
+  }, {
+    content: `Decorating the house`,
+    value: 5
+  }, {
+    content: `Drinking eggnog`,
+    value: 4
+  }, {
+    content: `Listening to Christmas music`,
+    value: 3
+  }, {
+    content: `Exchanging gifts`,
+    value: 3
+  }]
+}, {
+  q: `Name something you avoid during the holidays.`,
+  a: [{
+    content: `The mall`,
+    value: 34
+  }, {
+    content: `Shopping`,
+    value: 15
+  }, {
+    content: `Stores`,
+    value: 6
+  }, {
+    content: `Crowds`,
+    value: 6
+  }, {
+    content: `Scale`,
+    value: 5
+  }, {
+    content: `Black Friday`,
+    value: 4
+  }, {
+    content: `Traffic`,
+    value: 4
+  }]
+}, {
+  q: `Name a favoite holiday food.`,
+  a: [{
+    content: `Cookies`,
+    value: 18
+  }, {
+    content: `Ham`,
+    value: 16
+  }, {
+    content: `Fudge`,
+    value: 11
+  }, {
+    content: `Eggnog`,
+    value: 8
+  }, {
+    content: `Hot Chocolate`,
+    value: 6
+  }, {
+    content: `Tamales`,
+    value: 5
+  }, {
+    content: `Pie`,
+    value: 5
+  }, {
+    content: `Turkey`,
+    value: 5
+  }, {
+    content: `Prime Rib`,
+    value: 4
+  }]
+}, {
+  q: `Name something you wanted from Santa as a child.`,
+  a: [{
+    content: `Bike`,
+    value: 17
+  }, {
+    content: `Doll`,
+    value: 9
+  }, {
+    content: `Nintendo/Video Games`,
+    value: 8
+  }, {
+    content: `Cabbage Patch Doll`,
+    value: 7
+  }, {
+    content: `Dollhouse/Barbie Dream House`,
+    value: 6
+  }, {
+    content: `Action figures`,
+    value: 5
+  }, {
+    content: `Horse`,
+    value: 5
+  }, {
+    content: `Barbie`,
+    value: 5
+  }, {
+    content: `Rollerskates`,
+    value: 4
+  }, {
+    content: `Puppy`,
+    value: 3
+  }]
+}, {
+  q: `Name an item likely to be found in a Christmas stocking as a gift from Santa`,
+  a: [{
+    content: `Candy`,
+    value: 31
+  }, {
+    content: `Orange`,
+    value: 13
+  }, {
+    content: `Candy cane`,
+    value: 11
+  }, {
+    content: `Toiletries`,
+    value: 10
+  }, {
+    content: `Socks`,
+    value: 8
+  }, {
+    content: `Chocolate`,
+    value: 4
+  }, {
+    content: `Underwear`,
+    value: 3
+  }, {
+    content: `PEZ Dispenser`,
+    value: 3
+  }, {
+    content: `DVD Movie`,
+    value: 3
+  }]
+}, {
+  q: `Name a male movie star with whom one might like to be caught under the mistletoe.`,
+  a: [{
+    content: `Ryan Gosling`,
+    value: 7
+  }, {
+    content: `Matthew McConaughey`,
+    value: 6
+  }, {
+    content: `Hugh Jackman`,
+    value: 5
+  }, {
+    content: `George Clooney`,
+    value: 5
+  }, {
+    content: `Johnny Depp`,
+    value: 5
+  }, {
+    content: `Matt Damon`,
+    value: 4
+  }, {
+    content: `Patrick Dempsey`,
+    value: 4
+  }, {
+    content: `Ryan Reynolds`,
+    value: 4
+  }, {
+    content: `Christian Bale`,
+    value: 3
+  }, {
+    content: `Robert Pattinson`,
+    value: 3
+  }]
+}, {
+  q: `Name a female movie star with whom one might like to be caught under the mistletoe.`,
+  a: [{
+      content: `Jennifer Aniston`,
+      value: 5
+    }, {
+      content: `Angelina Jolie`,
+      value: 3
+    },
+    {
+      content: `Anne Hathaway`,
+      value: 3
+    },
+    {
+      content: `Sandra Bullock`,
+      value: 3
+    },
+    {
+      content: `Megan Fox`,
+      value: 3
+    },
+    {
+      content: `Salma Hayek`,
+      value: 2
+    },
+    {
+      content: `Mila Kunis`,
+      value: 2
+    },
+    {
+      content: `Betty White`,
+      value: 2
+    }
+  ]
+}, {
+  q: `Name one of Santa's reindeer`,
+  a: [{
+    content: `Rudolph`,
+    value: 50
+  }, {
+    content: `Dasher`,
+    value: 20
+  }, {
+    content: `Dancer`,
+    value: 9
+  }, {
+    content: `Blitzen`,
+    value: 7
+  }, {
+    content: `Prancer`,
+    value: 6
+  }, {
+    content: `Donder`,
+    value: 4
+  }, {
+    content: `Vixen`,
+    value: 2
+  }]
+}, {
+  q: `Name something you need to wrap a gift`,
+  a: [{
+    content: `Tape`,
+    value: 55
+  }, {
+    content: `Wrapping paper`,
+    value: 34
+  }, {
+    content: `The gift`,
+    value: 4
+  }, {
+    content: `Ribbon`,
+    value: 3
+  }, {
+    content: `Box`,
+    value: 2
+  }]
+}, {
+  q: `What is a popular holiday greeting`,
+  a: [{
+    content: `Merry Christmas`,
+    value: 78
+  }, {
+    content: `Happy Holidays`,
+    value: 17
+  }, {
+    content: `Season's Greetings`,
+    value: 3
+  }, {
+    content: `Feliz Navidad`,
+    value: 2
+  }]
+}, {
+  q: `Name one food you might have at a Christmas dinner.`,
+  a: [{
+    content: `Turkey`,
+    value: 63
+  }, {
+    content: `Ham`,
+    value: 25
+  }, {
+    content: `Mashed potatoes`,
+    value: 4
+  }, {
+    content: `Stuffing`,
+    value: 3
+  }, {
+    content: `Pie`,
+    value: 3
+  }, {
+    content: `Cranberries`,
+    value: 2
+  }]
+}, {
+  q: `Name a drink that might be served at a holiday party.`,
+  a: [{
+    content: `Eggnog`,
+    value: 71
+  }, {
+    content: `Punch`,
+    value: 8
+  }, {
+    content: `Cider`,
+    value: 8
+  }, {
+    content: `Wine`,
+    value: 3
+  }, {
+    content: `Wassail`,
+    value: 2
+  }, {
+    content: `Coffee`,
+    value: 2
+  }]
+}, {
+  q: `If you wanted to dress up like Santa, name one thing you would need.`,
+  a: [{
+    content: `Beard`,
+    value: 40
+  }, {
+    content: `Hat`,
+    value: 27
+  }, {
+    content: `Suit/Jacket`,
+    value: 19
+  }, {
+    content: `Boots`,
+    value: 6
+  }, {
+    content: `Big Belly`,
+    value: 3
+  }, {
+    content: `Belt`,
+    value: 3
+  }, {
+    content: `Pillow`,
+    value: 2
+  }]
+}, {
+  q: `Name a word or phrase that begins with "snow."`,
+  a: [{
+    content: `Snowflake`,
+    value: 30
+  }, {
+    content: `Snowman`,
+    value: 26
+  }, {
+    content: `Snowball`,
+    value: 23
+  }, {
+    content: `Snow White`,
+    value: 8
+  }, {
+    content: `Snow Angel`,
+    value: 3
+  }, {
+    content: `Snowfall`,
+    value: 3
+  }]
+}, {
+  q: `Name one color associated with Christmas`,
+  a: [{
+    content: `Red`,
+    value: 77
+  }, {
+    content: `Green`,
+    value: 16
+  }, {
+    content: `White`,
+    value: 5
+  }, {
+    content: `Gold`,
+    value: 2
+  }]
+}, {
+  q: `Name a Christmas movie featuring Santa`,
+  a: [{
+    content: `The Santa Clause (I, II, III)`,
+    value: 41
+  }, {
+    content: `Miracle on 34<sup>th</sup> St`,
+    value: 28
+  }, {
+    content: `Rudolph the Red Nosed Reindeer`,
+    value: 6
+  }, {
+    content: `Elf`,
+    value: 5
+  }, {
+    content: `Santa Claus is Coming to Town`,
+    value: 3
+  }, {
+    content: `A Christmas Story`,
+    value: 2
+  }, {
+    content: `Frosty the Snowman`,
+    value: 2
+  }, {
+    content: `Christmas Vacation`,
+    value: 2
+  }, {
+    content: `The Night Before Christmas`,
+    value: 2
+  }]
+}, {
+  q: `Name something people do on Christmas Day`,
+  a: [{
+    content: `Open Gifts`,
+    value: 71
+  }, {
+    content: `Eat`,
+    value: 17
+  }, {
+    content: `Go to church`,
+    value: 6
+  }, {
+    content: `Sing carols`,
+    value: 3
+  }, {
+    content: `Visit Family`,
+    value: 2
+  }]
+}, {
+  q: `Name a Christmas plant`,
+  a: [{
+    content: `Poinsettia`,
+    value: 71
+  }, {
+    content: `Christmas Cactus`,
+    value: 8
+  }, {
+    content: `Christmas Tree`,
+    value: 6
+  }, {
+    content: `Mistletoe`,
+    value: 6
+  }, {
+    content: `Holly`,
+    value: 6
+  }]
+}, {
+  q: `Name something you find at the North Pole`,
+  a: [{
+    content: `Snow`,
+    value: 34
+  }, {
+    content: `Elves`,
+    value: 22
+  }, {
+    content: `Santa Claus`,
+    value: 16
+  }, {
+    content: `Santa's Workshop`,
+    value: 9
+  }, {
+    content: `Reindeer`,
+    value: 5
+  }, {
+    content: `Ice`,
+    value: 4
+  }, {
+    content: `Mrs. Claus`,
+    value: 2
+  }, {
+    content: `Igloo`,
+    value: 2
+  }, {
+    content: `Toys`,
+    value: 2
+  }]
+}, {
+  q: `Name a Christmas cartoon that plays on TV every year`,
+  a: [{
+    content: `Rudolph the Red Nosed Reindeer`,
+    value: 29
+  }, {
+    content: `A Charlie Brown Christmas`,
+    value: 26
+  }, {
+    content: `Frosty the Snowman`,
+    value: 19
+  }, {
+    content: `How the Grinch Stole Christmas`,
+    value: 17
+  }, {
+    content: `Santa Claus is Coming to Town`,
+    value: 3
+  }]
+}, {
+  q: `Name something you hang on a Christmas tree.`,
+  a: [{
+    content: `Lights`,
+    value: 25
+  }, {
+    content: `Ornaments`,
+    value: 23
+  }, {
+    content: `Angel`,
+    value: 10
+  }, {
+    content: `Tinsel`,
+    value: 10
+  }, {
+    content: `Candy Cane`,
+    value: 7
+  }]
+}]
+
+
+
+, {
+  q: ``,
+  a: [{
+    content: ``,
+    value:
+  }, ]
+}
