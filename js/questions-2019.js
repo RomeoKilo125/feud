667df95001f1078509d886b8d0962f58b7943857
RomeoKilo125 <romeokilo125@gmail.com>
2019-07-03 19:06:35 -0400

    began adding questions for customer service week 2019

---
+++ js/questions-2019.js
@@ -0,0 +1,103 @@
+let questions = [
+  {
+    q: "Name something that often has magical powers in fairy tales.",
+    a: [
+      {
+        content: "Wand",
+        value: 34
+      }, {
+        content: "Witch",
+        value: 24
+      }, {
+        content: "Fairy Godmother",
+        value: 18
+      }, {
+        content: "Wizard",
+        value: 12
+      }, {
+        content: "Fairy Dust",
+        value: 4
+      }, {
+        content: "Broom",
+        value: 4
+      }
+    ]
+  }, {
+    q: "Which magic trick would become dangerous if the magician was a phony?",
+    a: [
+      {
+        content: "Person cut in half",
+        value: 52
+      }, {
+        content: "Sword Swallow",
+        value: 19
+      }, {
+        content: "disappearing act",
+        value: 14
+      }, {
+        content: "knife throwing",
+        value: 11
+      }
+    ]
+  }, {
+    q: "which word or phrase is often used as a magicians magic word(s)",
+    a: [
+      {
+        content: "abra cadabra",
+        value: 51
+      }, {
+        content: "hocus pocus",
+        value: 24
+      }, {
+        content: "alakazam",
+        value: 12
+      }, {
+        content: "presto",
+        value: 9
+      }
+    ]
+  }, {
+    q: "if there were a college for magicians, name a trick you're sure would be taught on day one.",
+    a: [
+      {
+        content: "objects disappear",
+        value: 37
+      }, {
+        content: "rabbit out of a hat",
+        value: 25
+      }, {
+        content: "card tricks",
+        value: 19
+      }, {
+        content: "saw in half",
+        value: 19
+      }
+    ]
+  }, {
+    q: "past or present, name a famout magician",
+    a: [
+      {
+        content: "harry houdini",
+        value: 60
+      }, {
+        content: "david copperfield",
+        value: 17
+      }, {
+        content: "merlin",
+        value: 14
+      }, {
+        content: "david blaine",
+        value: 7
+      }, {
+        content: "",
+        value:
+      }, {
+        content: "",
+        value:
+      }, {
+        content: "",
+        value:
+      }
+    ]
+  }
+]
