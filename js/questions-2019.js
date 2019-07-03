aa8a62d742377d063ceff22ea5f038912f6c72b7
RomeoKilo125 <romeokilo125@gmail.com>
2019-07-03 19:53:07 -0400

    more questions for 2019, I think there's a theme forming here.

--- js/questions-2019.js
+++ js/questions-2019.js
@@ -74,7 +74,7 @@ let questions = [
       }
     ]
   }, {
-    q: "past or present, name a famout magician",
+    q: "past or present, name a famous magician",
     a: [
       {
         content: "harry houdini",
@@ -88,6 +88,246 @@ let questions = [
       }, {
         content: "david blaine",
         value: 7
+      }
+    ]
+  }, {
+    q: "name a color leaves turn in autumn",
+    a: [
+      {
+        content: "red",
+        value: 27
+      }, {
+        content: "brown",
+        value: 23
+      }, {
+        content: "orange",
+        value: 19
+      }, {
+        content: "yellow",
+        value: 17
+      }, {
+        content: "gold",
+        value: 12
+      }, {
+        content: "crimson",
+        value: 2
+      }
+    ]
+  }, {
+    q: "name something bad that might happen to a magician's assistant during a performance",
+    a: [
+      {
+        content: "cut in half",
+        value: 46
+      }, {
+        content: "disappear",
+        value: 25
+      }, {
+        content: "trip",
+        value: 14
+      }, {
+        content: "get hit by a knife",
+        value: 13
+      }
+    ]
+  }, {
+    q: "name a smell you associate with fall",
+    a: [
+      {
+        content: "leaves",
+        value: 46
+      }, {
+        content: "rain",
+        value: 20
+      }, {
+        content: "campfire",
+        value: 9
+      }, {
+        content: "pumpkin pie",
+        value: 9
+      }, {
+        content: "apples",
+        value: 5
+      }
+    ]
+  }, {
+    q: "name something a migician might make disappear",
+    a: [
+      {
+        content: "rabbit",
+        value: 43
+      }, {
+        content: "assistant/spectator",
+        value: 36
+      }, {
+        content: "bird",
+        value: 7
+      }, {
+        content: "money",
+        value: 4
+      }, {
+        content: "flowers",
+        value: 3
+      }, {
+        content: "themselves",
+        value: 3
+      }
+    ]
+  }, {
+    q: "name something a child needs to look like a princess",
+    a: [
+      {
+        content: "tiara",
+        value: 63
+      }, {
+        content: "dress",
+        value: 17
+      }, {
+        content: "magic wand",
+        value: 5
+      }, {
+        content: "makeup",
+        value: 4
+      }, {
+        content: "jewelry",
+        value: 3
+      }, {
+        content: "charm",
+        value: 2
+      }
+    ]
+  }, {
+    q: "name a sport that is played during the fall",
+    a: [
+      {
+        content: "football",
+        value: 56
+      }, {
+        content: "hockey",
+        value: 13
+      }, {
+        content: "basketball",
+        value: 13
+      }
+    ]
+  }, {
+    q: "name a famous person who's last name is \"Johnson\"",
+    a: [
+      {
+        content: "magic johnson",
+        value: 34
+      }, {
+        content: "lyndon b johnson",
+        value: 13
+      }, {
+        content: "don johnson",
+        value: 13
+      }, {
+        content: "andrew johnson",
+        value: 8
+      }, {
+        content: "howard johnson",
+        value: 7
+      }, {
+        content: "lady bird johnson",
+        value: 5
+      }
+    ]
+  }, {
+    q: "other than an airplane, what might a cartoon character use to help them fly",
+    a: [
+      {
+        content: "wings",
+        value: 46
+      }, {
+        content: "arms",
+        value: 18
+      }, {
+        content: "bird",
+        value: 13
+      }, {
+        content: "balloon",
+        value: 8
+      }, {
+        content: "kite",
+        value: 4
+      }, {
+        content: "magic carpet",
+        value: 4
+      }
+    ]
+  }, {
+    q: "if you were offered a magic carpet ride, name something you'd add to the trip more comfy",
+    a: [
+      {
+        content: "seat/cushion",
+        value: 53
+      }, {
+        content: "refreshments",
+        value: 15
+      }, {
+        content: "safety belt",
+        value: 11
+      }, {
+        content: "tv",
+        value: 7
+      }, {
+        content: "blanket",
+        value: 5
+      }, {
+        content: "bathroom",
+        value: 4
+      }
+    ]
+  }, {
+    q: "name something a magician might remove from audience members without them knowing it was gone",
+    a: [
+      {
+        content: "money",
+        value: 29
+      }, {
+        content: "watch",
+        value: 26
+      }, {
+        content: "wallet/purse",
+        value: 14
+      }, {
+        content: "handkerchief",
+        value: 12
+      }, {
+        content: "necklace",
+        value: 8
+      }, {
+        content: "ring",
+        value: 6
+      }
+    ]
+  }, {
+    q: "what might you name your son if you wanted him to grow up to be a magician?",
+    a: [
+      {
+        content: "Houdini",
+        value:49
+      }, {
+        content: "Merlin",
+        value:30
+      }
+    ]
+  }, {
+    q: "",
+    a: [
+      {
+        content: "",
+        value:
+      }, {
+        content: "",
+        value:
+      }, {
+        content: "",
+        value:
+      }, {
+        content: "",
+        value:
       }, {
         content: "",
         value:
